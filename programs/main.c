505356a8f950acf0ff3ffb2349cdfafc5fb1b694
rutvi19 <rutvimandaliya19@gmail.com>
2025-04-30 22:50:39 +0530

    commit

---
+++ programs/main.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    
+    printf("\n your name: rutvi mandaliya");
+    printf("\n your birth date: 19-02-2006");
+    printf("\n your age: 19");
+    printf("\n your address: rajkot");
+    getch();
+}
+
