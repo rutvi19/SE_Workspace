3032f48271bf9bbec9c17fadb15eeafeefb1064c
rutvi19 <rutvimandaliya19@gmail.com>
2025-04-30 23:00:51 +0530

    commit

---
+++ programs/main1.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int a=2,b=3;
+    printf("\n the addtion is %d",a+b);
+    printf("\n the subtraction is %d",a-b);
+    printf("\n the multiplication is %d",a*b);
+    printf("\n the division is %f",a/b);
+    printf("\n the modulas is %d",a%b);
+    return 0;
+}
