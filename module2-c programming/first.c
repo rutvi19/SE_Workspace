41c9984f4f13a95e45411c72c8ead754f1df21c1
rutvi19 <rutvimandaliya19@gmail.com>
2025-05-01 11:27:18 +0530

    commit

---
+++ module2-c programming/first.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    printf("\n hello rutviii..");
+    printf("\n hello prakruti mam...");
+
+    return 0;
+}
