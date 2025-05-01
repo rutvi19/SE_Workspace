664901d44b6cec0f8160a7ac2a45d597f4509291
rutvi19 <rutvimandaliya19@gmail.com>
2025-05-01 11:12:28 +0530

    commit

---
+++ module2-c programming/main8.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int num = 90;
+    float num2=90.80;
+    char c1='R';
+    double num3=84.666;
+    printf("\n your number is %d",num);
+    printf("\n your number is %f",num2);
+    printf("\n your number is %lf",num3);
+    printf("\n your number is %c",c1);
+
+    return 0;
+}
