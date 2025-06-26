08692007097904349e8fe9dbe349ede475a9c6a3
rutvi19 <rutvimandaliya19@gmail.com>
2025-06-26 17:18:13 +0530

    commit

--- assingment/module-3 assingment/6.intro to oop/inheritance.cpp
+++ assingment/module-3 assingment/6.intro to oop/inheritance.cpp
@@ -1,48 +1,66 @@
-#include <iostream>
+
+#include<iostream>
 using namespace std;
 
-class person
+class Shape
 {
-    public:
-    int age = 20;
+    public:  
+    int a;  
+    int b; 
     
     public:
-    void show1()
-    {
-        cout<<age<<endl;
-    }
-};
-class student : public person
-{
-    public:
-    int age1 = 19;
-    
-    public:
-    void show2()
-    {
-        cout<<age1<<endl;
-    }
-    
-};
-class teacher : public student
-{
-    public:
-    int age2 = 18;
-    
-    public:
-    void show3()
-    {
-        cout<<age2<<endl;
+    void get_data(int n,int m)  
+    {  
+        a= n;  
+        b = m;  
     }  
+    
+    
 };
+
+class Rec : public Shape
+{
+    public:
+    int rec_area()
+    {
+        int result = a*b;  
+        return result;  
+    }
+};
+class Triangle : public Shape
+{
+      public:
+     int tri_area()
+    {
+        float result = 0.5*a*b;  
+        return result;  
+    }
+};
+
+
+
 int main()
 {
+    int length,breadth,base,height;
     
+    //Rec
     
-    teacher a = teacher();
-    a.show1();
-    a.show2();
-    a.show3();
+    cout << "Enter the length and breadth of a rectangle: ";
+    cin>>length>>breadth;
+    Rec r = Rec();
+    r.get_data(length,breadth);
+    int m = r.rec_area();
+    
+    //Tri
+    cout << "Enter the base and height of the triangle: "<<endl;  
+    cin>>base>>height;  
+    Triangle t = Triangle();
+    t.get_data(base,height);
+    //t.tri_area();
+    int n = t.tri_area();
+    
+    cout<<"Area of Rectangle is : "<<m<<endl;
+    cout<<"Area of Triangle is : "<<n<<endl;
     
     
     return 0;
