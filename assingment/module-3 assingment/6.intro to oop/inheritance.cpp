fec10ca9425187b7e044b60be33c8044a0fc69bf
rutvi19 <rutvimandaliya19@gmail.com>
2025-06-27 16:49:09 +0530

    commit

--- assingment/module-3 assingment/6.intro to oop/inheritance.cpp
+++ assingment/module-3 assingment/6.intro to oop/inheritance.cpp
@@ -1,67 +1,52 @@
-
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Shape
+class person
 {
-    public:  
-    int a;  
-    int b; 
-    
-    public:
-    void get_data(int n,int m)  
-    {  
-        a= n;  
-        b = m;  
-    }  
-    
-    
+	public:
+    string name;
+    int age;
 };
+class teacher : public person
+{
+    public:
+    string add;
+    int sal;
 
-class Rec : public Shape
-{
-    public:
-    int rec_area()
+    int display()
     {
-        int result = a*b;  
-        return result;  
-    }
-};
-class Triangle : public Shape
-{
-      public:
-     int tri_area()
-    {
-        float result = 0.5*a*b;  
-        return result;  
+        cout << "enter name : ";
+        cin >> name;
+        cout << "enter age : ";
+        cin >> age;
+        cout << "enter address : ";
+        cin >> add;
+        cout << "enter salery : ";
+        cin >> sal;
     }
 };
 
+class student : public person
+{
+    public:
+    string name1;
+    int rollno;
 
+    int display()
+    {
+        
+        cout << "enter name of student : ";
+        cin >> name1;
+        cout << "enter name : ";
+        cin >> rollno;
+    }
+};
 
 int main()
 {
-    int length,breadth,base,height;
-    
-    //Rec
-    
-    cout << "Enter the length and breadth of a rectangle: ";
-    cin>>length>>breadth;
-    Rec r = Rec();
-    r.get_data(length,breadth);
-    int m = r.rec_area();
-    
-    //Tri
-    cout << "Enter the base and height of the triangle: "<<endl;  
-    cin>>base>>height;  
-    Triangle t = Triangle();
-    t.get_data(base,height);
-    //t.tri_area();
-    int n = t.tri_area();
-    
-    cout<<"Area of Rectangle is : "<<m<<endl;
-    cout<<"Area of Triangle is : "<<n<<endl;
-    
-    
-    return 0;
+    teacher t = teacher();
+    student s = student();
+
+    t.display();
+    s.display();
 }
